678a520ddc7b7c14c8fb6a2c1a9178314805b9b3
Kacper Słomiński <kacper.slominski72@gmail.com>
2023-06-20 19:48:55 +0200

    toolchain: Reformat code

--- toolchain/as-helper/relocate.scm
+++ toolchain/as-helper/relocate.scm
@@ -2,61 +2,60 @@
 (use-modules (ice-9 match) (srfi srfi-1) (srfi srfi-26) (as-helper utility))
 
 (define (relocate-label base-addr label-cell)
- (cons (car label-cell) (+ base-addr (cdr label-cell))))
+  (cons (car label-cell) (+ base-addr (cdr label-cell))))
 
 (define (lookup-in-env name env)
- (match (assoc name env)
-  ((k . v) v)
-  (_ (error "Name not found in environment:" name))))
+  (match (assoc name env)
+    ((k . v) v)
+    (_ (error "Name not found in environment:" name))))
+
 
 (define (eval-reloc-expr pc stack-off expr env)
- (match expr
-  ('$ pc)
-  ('$frame stack-off)
-  ((? number? val) val)
-  ((? string? val) val)
-  ((? symbol? name) (lookup-in-env name env))
-  (('bank expr) (logand (ash (eval-reloc-expr pc stack-off expr env) -16) #xff))
-  (('local-addr expr) (logand (eval-reloc-expr pc stack-off expr env) #xffff))
-  (('in-frame expr) (+ (eval-reloc-expr pc stack-off expr env) stack-off))
-  (('+ expr by) (+ (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('- expr by) (- (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('<< expr by) (ash (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('>> expr by) (ash (eval-reloc-expr pc stack-off expr env) (- (eval-reloc-expr pc stack-off by env))))
-  (('| expr by) (logior (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('& expr by) (logand (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('^ expr by) (logxor (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
-  (('far bank addr) (logior (ash (eval-reloc-expr pc stack-off bank env) 16) (eval-reloc-expr pc stack-off addr env)))))
+  (match expr
+    ('$ pc)
+    ('$frame stack-off)
+    ((? number? val) val)
+    ((? string? val) val)
+    ((? symbol? name) (lookup-in-env name env))
+    (('bank expr) (logand (ash (eval-reloc-expr pc stack-off expr env) -16) #xff))
+    (('local-addr expr) (logand (eval-reloc-expr pc stack-off expr env) #xffff))
+    (('in-frame expr) (+ (eval-reloc-expr pc stack-off expr env) stack-off))
+    (('+ expr by) (+ (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('- expr by) (- (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('<< expr by) (ash (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('>> expr by) (ash (eval-reloc-expr pc stack-off expr env) (- (eval-reloc-expr pc stack-off by env))))
+    (('| expr by) (logior (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('& expr by) (logand (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('^ expr by) (logxor (eval-reloc-expr pc stack-off expr env) (eval-reloc-expr pc stack-off by env)))
+    (('far bank addr) (logior (ash (eval-reloc-expr pc stack-off bank env) 16) (eval-reloc-expr pc stack-off addr env)))))
 
 (define (apply-patch-to-bytes bytes off patch)
- `(,@(take bytes off) ,@patch ,@(drop bytes (+ (length patch) off))))
+  `(,@(take bytes off) ,@patch ,@(drop bytes (+ (length patch) off))))
 
 (define (relocate base-addr global-env bytes relocs local-env)
- (let ((env `(,@global-env
-              ,@(map (cut relocate-label base-addr <>) local-env)))
-       (cur-relocs relocs) (cur-bytes bytes))
-  (while (not (null? cur-relocs))
-   (match (car cur-relocs)
-    (('reloc-abs offset stack-off sz expr)
-     (set! cur-bytes
-      (apply-patch-to-bytes
-       cur-bytes offset
-       (value-to-n-bytes (eval-reloc-expr
-                          (+ offset base-addr -1) stack-off expr env) sz))))
-    (('reloc-frame-rel offset stack-off sz expr)
-     (set! cur-bytes
-      (apply-patch-to-bytes
-       cur-bytes offset
-       (value-to-n-bytes (+ stack-off (eval-reloc-expr
-                                       (+ offset base-addr -1) stack-off expr env)) sz))))
-    (('reloc-branch offset stack-off sz expr)
-     (set! cur-bytes
-      (apply-patch-to-bytes
-       cur-bytes offset
-       (compute-branch-target
-        (+ offset base-addr) sz
-        (eval-reloc-expr (+ offset base-addr -1) stack-off expr env))))))
-   (set! cur-relocs (cdr cur-relocs)))
-  cur-bytes))
-
-
+  (let ((env `(,@global-env
+	       ,@(map (cut relocate-label base-addr <>) local-env)))
+	(cur-relocs relocs) (cur-bytes bytes))
+    (while (not (null? cur-relocs))
+      (match (car cur-relocs)
+	(('reloc-abs offset stack-off sz expr)
+	 (set! cur-bytes
+	       (apply-patch-to-bytes
+		cur-bytes offset
+		(value-to-n-bytes (eval-reloc-expr
+				   (+ offset base-addr -1) stack-off expr env) sz))))
+	(('reloc-frame-rel offset stack-off sz expr)
+	 (set! cur-bytes
+	       (apply-patch-to-bytes
+		cur-bytes offset
+		(value-to-n-bytes (+ stack-off (eval-reloc-expr
+						(+ offset base-addr -1) stack-off expr env)) sz))))
+	(('reloc-branch offset stack-off sz expr)
+	 (set! cur-bytes
+	       (apply-patch-to-bytes
+		cur-bytes offset
+		(compute-branch-target
+		 (+ offset base-addr) sz
+		 (eval-reloc-expr (+ offset base-addr -1) stack-off expr env))))))
+      (set! cur-relocs (cdr cur-relocs)))
+    cur-bytes))
--- toolchain/as-helper/toplevel.scm
+++ toolchain/as-helper/toplevel.scm
@@ -1,93 +1,91 @@
 (define-module (as-helper toplevel) #: export (proc far-proc extern flat-binary))
 (use-modules (ice-9 match) (srfi srfi-26) (as-helper relocate)
-             (as-helper assemble) (as-helper utility))
+	     (as-helper assemble) (as-helper utility))
 
 (define (build-frame-env return-size params locals)
- (let ((items `(,@locals (!!return-addr . ,return-size) ,@params))
-       (cur-offset 1)
-       (env '()))
-  (while (not (null? items))
-   (set! env `(,@env (,(caar items) . ,cur-offset)))
-   (set! cur-offset (+ cur-offset (cdar items)))
-   (set! items (cdr items)))
-  env))
+  (let ((items `(,@locals (!!return-addr . ,return-size) ,@params))
+	(cur-offset 1)
+	(env '()))
+    (while (not (null? items))
+      (set! env `(,@env (,(caar items) . ,cur-offset)))
+      (set! cur-offset (+ cur-offset (cdar items)))
+      (set! items (cdr items)))
+    env))
 
 (define-syntax define-proc-syntax
- (syntax-rules ::: ()
-  ((_ proc-kind proc-ret-size)
-   (define-syntax proc-kind
-    (syntax-rules ()
-     ((_ name params locals body ...)
-      (match-let (((bytes relocs labels) (assemble `(body ...))))
-       `(,'name . ((data . ,bytes)
-                    (local-relocs . ,relocs)
-                    (local-labels . ,labels)
-                    (priv-env . ,(build-frame-env proc-ret-size `params `locals)))))))))))
+  (syntax-rules ::: ()
+    ((_ proc-kind proc-ret-size)
+     (define-syntax proc-kind
+       (syntax-rules ()
+	 ((_ name params locals body ...)
+	  (match-let (((bytes relocs labels) (assemble `(body ...))))
+	    `(,'name . ((data . ,bytes)
+			(local-relocs . ,relocs)
+			(local-labels . ,labels)
+			(priv-env . ,(build-frame-env proc-ret-size `params `locals)))))))))))
 
 (define-proc-syntax proc 2)
 (define-proc-syntax far-proc 3)
 
 (define-syntax extern
- (syntax-rules ()
-  ((_ name addr)
-   `(() . ((pub-env . (,'name . ,addr)))))))
+  (syntax-rules ()
+    ((_ name addr)
+     `(() . ((pub-env . (,'name . ,addr)))))))
 
 (define (offset-relocation reloc by)
- (match reloc
-  (('reloc-abs offset stack-off sz expr)
-   `(reloc-abs ,(+ by offset) ,stack-off ,sz ,expr))
-  (('reloc-frame-rel offset stack-off sz expr)
-   `(reloc-frame-rel ,(+ by offset) ,stack-off ,sz ,expr))
-  (('reloc-branch offset stack-off sz expr)
-   `(reloc-branch ,(+ by offset) ,stack-off ,sz ,expr))))
+  (match reloc
+    (('reloc-abs offset stack-off sz expr)
+     `(reloc-abs ,(+ by offset) ,stack-off ,sz ,expr))
+    (('reloc-frame-rel offset stack-off sz expr)
+     `(reloc-frame-rel ,(+ by offset) ,stack-off ,sz ,expr))
+    (('reloc-branch offset stack-off sz expr)
+     `(reloc-branch ,(+ by offset) ,stack-off ,sz ,expr))))
 
 (define (offset-label label-cell by)
- (cons (car label-cell) (+ by (cdr label-cell))))
+  (cons (car label-cell) (+ by (cdr label-cell))))
 
 (define (append-entry data entry)
- (let ((offset (length data)) (entry-name (car entry)) (entry-data (cdr entry)))
-  `(,(append data (assoc-get-or-default 'data entry-data '()))
-    (,entry-name
-     (offset . ,offset)
-     (data . ,(assoc-get-or-default 'data entry-data '()))
-     (local-relocs . ,(map (cut offset-relocation <> offset) (assoc-get-or-default 'local-relocs entry-data '())))
-     (local-labels . ,(map (cut offset-label <> offset) (assoc-get-or-default 'local-labels entry-data '())))
-     (priv-env . ,(assoc-get-or-default 'priv-env entry-data '()))
-     (pub-env  . ,(assoc-get-or-default 'pub-env entry-data '()))))))
+  (let ((offset (length data)) (entry-name (car entry)) (entry-data (cdr entry)))
+    `(,(append data (assoc-get-or-default 'data entry-data '()))
+      (,entry-name
+       (offset . ,offset)
+       (data . ,(assoc-get-or-default 'data entry-data '()))
+       (local-relocs . ,(map (cut offset-relocation <> offset) (assoc-get-or-default 'local-relocs entry-data '())))
+       (local-labels . ,(map (cut offset-label <> offset) (assoc-get-or-default 'local-labels entry-data '())))
+       (priv-env . ,(assoc-get-or-default 'priv-env entry-data '()))
+       (pub-env  . ,(assoc-get-or-default 'pub-env entry-data '()))))))
 
 (define (nonnull-in-list? table key)
- (match (assoc key table)
-  ((k . v) (not (null? v))) (_ #f)))
+  (match (assoc key table)
+    ((k . v) (not (null? v))) (_ #f)))
 
 (define (build-global-symbol-table base entries)
- (map-in-order
-  (lambda (entry)
-   (if (nonnull-in-list? (cdr entry) 'pub-env)
-    (assoc-get-or-default 'pub-env (cdr entry) '())
-    `(,(car entry) . ,(+ base (assoc-get-or-default 'offset (cdr entry) 0)))))
-  entries))
+  (map-in-order
+   (lambda (entry)
+     (if (nonnull-in-list? (cdr entry) 'pub-env)
+	 (assoc-get-or-default 'pub-env (cdr entry) '())
+	 `(,(car entry) . ,(+ base (assoc-get-or-default 'offset (cdr entry) 0)))))
+   entries))
 
 (define (flat-binary base-addr . entries)
- (let ((data '()) (actual-entries '()))
-  (for-each
-   (lambda (entry)
-    (match-let (((new-data new-entry) (append-entry data entry)))
-     (set! actual-entries (cons new-entry actual-entries))
-     (set! data new-data)))
-   entries)
+  (let ((data '()) (actual-entries '()))
+    (for-each
+     (lambda (entry)
+       (match-let (((new-data new-entry) (append-entry data entry)))
+	 (set! actual-entries (cons new-entry actual-entries))
+	 (set! data new-data)))
+     entries)
 
-  (let ((symbol-table (build-global-symbol-table base-addr actual-entries)))
-   (for-each
-    (lambda (entry)
-     (set! data
-      (relocate
-       base-addr
-       `(,@symbol-table
-         ,@(assoc-get-or-default 'priv-env (cdr entry) '()))
-       data
-       (assoc-get-or-default 'local-relocs (cdr entry) '())
-       (assoc-get-or-default 'local-labels (cdr entry) '()))))
-    actual-entries)
-   data)))
-
-
+    (let ((symbol-table (build-global-symbol-table base-addr actual-entries)))
+      (for-each
+       (lambda (entry)
+	 (set! data
+	       (relocate
+		base-addr
+		`(,@symbol-table
+		  ,@(assoc-get-or-default 'priv-env (cdr entry) '()))
+		data
+		(assoc-get-or-default 'local-relocs (cdr entry) '())
+		(assoc-get-or-default 'local-labels (cdr entry) '()))))
+       actual-entries)
+      data)))
