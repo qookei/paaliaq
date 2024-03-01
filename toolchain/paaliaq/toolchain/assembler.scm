9855d82794e9a5ee9d1e4c804046495fb0004359
Kacper Słomiński <kacper.slominski72@gmail.com>
2024-03-01 23:43:51 +0100

    toolchain: Update year and clarify comment in assembler

--- toolchain/paaliaq/toolchain/assembler.scm
+++ toolchain/paaliaq/toolchain/assembler.scm
@@ -7,7 +7,7 @@
 (define (version)
   (display "\
 as (paaliaq as) 0.1
-Copyright (C) 2023  qookie
+Copyright (C) 2024  qookie
 License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
