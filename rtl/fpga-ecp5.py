959d602a5ccc0982b6e205d6f9363a0587828380
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-05-11 15:11:28 +0200

    rtl: Compute PLL parameters dynamically
    Logic is derived from ecppll as noted in the comment.

--- rtl/fpga-ecp5.py
+++ rtl/fpga-ecp5.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, math
 
 from amaranth import *
 from amaranth.lib import wiring
@@ -11,6 +11,79 @@ from uart import UARTTransmitter
 from probe import W65C816DebugProbe
 from sdram import SDRAMConnector
 from cpu import W65C816Connector, P65C816SoftCore
+
+
+# Compute PLL parameters for a PLL with a primary and one secondary
+# output configured to the same frequency, with the secondary output
+# being 180 degrees out of phase with the primary.
+# Logic derived from the ecppll tool from prjtrellis:
+# https://github.com/YosysHQ/prjtrellis/blob/master/libtrellis/tools/ecppll.cpp
+# (specifically calc_pll_params, generate_secondary_output)
+PFD_MIN = 3.125
+PFD_MAX = 400
+VCO_MIN = 400
+VCO_MAX = 800
+def compute_pll_params(in_clk, out_clk):
+    in_MHz = in_clk // 1e6
+    out_MHz = out_clk // 1e6
+
+    assert in_MHz * 1e6 == in_clk, 'Input clock must be a integer multiple of MHz'
+    assert out_MHz * 1e6 == out_clk, 'Output clock must be a integer multiple of MHz'
+
+    best_in_div = -1
+    best_fb_div = -1
+    best_out_div = -1
+    best_fvco = -1
+    best_fout = -1
+
+    error = math.inf
+    for in_div in range(1, 129):
+        fpfd = in_MHz / in_div
+        if fpfd < PFD_MIN or fpfd > PFD_MAX:
+            continue
+        for fb_div in range(1, 81):
+            for out_div in range(1, 129):
+                fvco = fpfd * fb_div * out_div
+                if fvco < VCO_MIN or fvco > VCO_MAX:
+                    continue
+                fout = fvco / out_div
+                if abs(fout - out_MHz) < error or (
+                        abs(fout - out_MHz) == error
+                        and abs(fvco - 600) < abs(best_fvco - 600)
+                ):
+                    error = abs(fout - out_MHz)
+                    best_in_div = in_div
+                    best_fb_div = fb_div
+                    best_out_div = out_div
+                    best_fvco = fvco
+                    best_fout = fout
+
+    assert best_fout == out_MHz, 'Failed to find PLL configuration that reaches target freq.'
+
+    # Primary clock has a phase shift of 180 degrees.  Secondary clock
+    # has a phase shift of 180 degrees relative to primary.
+    ns_shift = 1 / (out_MHz * 1e6) * (180 / 360)
+    primary_cphase = int(ns_shift * (best_fvco * 1e6))
+    secondary_cphase = int(primary_cphase * 2)
+
+    return {
+        'a_FREQUENCY_PIN_CLKI': int(in_MHz),
+        'a_FREQUENCY_PIN_CLKOP': int(out_MHz),
+        'a_FREQUENCY_PIN_CLKOS': int(out_MHz),
+        'a_ICP_CURRENT': 12,
+        'a_LPF_RESISTOR': 8,
+        'p_CLKI_DIV': best_in_div,
+        'p_FEEDBK_PATH': 'CLKOP',
+        'p_CLKFB_DIV': best_fb_div,
+        'p_CLKOP_ENABLE': 'ENABLED',
+        'p_CLKOP_DIV': best_out_div,
+        'p_CLKOP_CPHASE': primary_cphase,
+        'p_CLKOP_FPHASE': 0,
+        'p_CLKOS_ENABLE': 'ENABLED',
+        'p_CLKOS_DIV': best_out_div,
+        'p_CLKOS_CPHASE': secondary_cphase,
+        'p_CLKOS_FPHASE': 0,
+    }
 
 
 class FpgaTopLevel(Elaboratable):
@@ -34,27 +107,9 @@ class FpgaTopLevel(Elaboratable):
         # TODO: (Bug amaranth-lang/amaranth#1565).
         # platform.add_clock_constraint(cd_sync.clk, self._target_clk)
 
-        # TODO(qookie): Compute the PLL parameters dynamically?
-        assert self._target_clk == 75e6, 'Adjust PLL parameters for new clock speed'
         m.submodules.pll = Instance(
             "EHXPLLL",
-
-            a_FREQUENCY_PIN_CLKI="25",
-            a_FREQUENCY_PIN_CLKOP="75",
-            a_FREQUENCY_PIN_CLKOS="75",
-            a_ICP_CURRENT="12",
-            a_LPF_RESISTOR="8",
-            p_CLKI_DIV=1,
-            p_CLKOP_DIV=8,
-            p_CLKOS_DIV=8,
-            p_CLKOP_CPHASE=4,
-            p_CLKOP_FPHASE=0,
-            p_CLKOS_CPHASE=8,
-            p_CLKOS_FPHASE=0,
-            p_CLKFB_DIV=3,
-            p_FEEDBK_PATH="CLKOP",
-            p_CLKOP_ENABLE="ENABLED",
-            p_CLKOS_ENABLE="ENABLED",
+            **compute_pll_params(platform.default_clk_frequency, self._target_clk),
 
             i_CLKI=platform.request("clk25").i,
 
