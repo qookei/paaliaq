516ad56a2d55d8873a1af3afe035d8861d539354
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-05-18 15:04:06 +0200

    rtl: Begin work on an JTAG debug interface
    This commit adds an implementation of the core JTAG interface based on
    the ECP5 JTAGG primitive. Actual debug interface is to be done,
    currently all that is done is just piping all incoming data into the
    output FIFO.

---
+++ rtl/jtag.py
@@ -0,0 +1,140 @@
+from amaranth import *
+from amaranth.lib import wiring
+from amaranth.lib.fifo import AsyncFIFOBuffered
+from amaranth.lib.wiring import In, Out
+from amaranth_soc import wishbone
+
+
+# Based (loosely) on the implementations from LiteX and Hazard3:
+# https://github.com/enjoy-digital/litex/blob/master/litex/soc/cores/jtag.py
+# https://github.com/Wren6991/Hazard3/blob/stable/hdl/debug/dtm/hazard3_ecp5_jtag_dtm.v
+#
+# Uses the same hack as the Hazard3 impl, where all the logic is done
+# on (or near) TCK negedge by inverting TCK.  To account for JTDO{1,2}
+# being registered on TCK negedge, we have the first data bit ready
+# after JCE{1,2} goes high (=> in Capture-DR state). To account for
+# TDI being registered, we also do shifting in Update-DR, and instead
+# use the negative edge of Update-DR to trigger a FIFO write.
+#
+# The interface this component provides is simple: two FIFOs buffering
+# the incoming and outgoing data, with ER2 being the dedicated input
+# register, and ER1 being the dedicated output register.
+class JTAGG(wiring.Component):
+    tx_data: In(8)
+    tx_rdy: Out(1)
+    tx_stb: In(1)
+
+    rx_data: Out(8)
+    rx_rdy: Out(1)
+    rx_stb: In(1)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        tck = Signal()
+
+        tdo = Signal()
+        tdi = Signal()
+
+        jce1, jce1_d = Signal(), Signal()
+        jce2, jce2_d = Signal(), Signal()
+        shift, update = Signal(), Signal()
+
+        capture1 = jce1 & ~jce1_d
+        capture2 = jce2 & ~jce2_d
+
+        m.d.jtag += jce1_d.eq(jce1)
+        m.d.jtag += jce2_d.eq(jce2)
+
+        m.submodules.jtagg = jtagg = Instance(
+            "JTAGG",
+            o_JTCK=tck,
+            i_JTDO1=tdo,
+            i_JTDO2=tdo,
+            o_JTDI=tdi,
+            o_JSHIFT=shift,
+            o_JUPDATE=update,
+            o_JCE1=jce1,
+            o_JCE2=jce2,
+        )
+
+        cd_jtag = ClockDomain("jtag", local=True)
+        m.domains += cd_jtag
+
+        m.d.comb += cd_jtag.clk.eq(~tck)
+        m.submodules += cdc.ResetSynchronizer(arst=ResetSignal(), domain="jtag")
+
+        m.submodules.tx_fifo = tx_fifo = AsyncFIFOBuffered(
+            width=8,
+            depth=16,
+            r_domain="jtag",
+            w_domain="sync")
+
+        m.d.comb += [
+            tx_fifo.w_data.eq(self.tx_data),
+            tx_fifo.w_en.eq(self.tx_stb),
+            self.tx_rdy.eq(tx_fifo.w_rdy),
+        ]
+
+        m.submodules.rx_fifo = rx_fifo = AsyncFIFOBuffered(
+            width=8,
+            depth=16,
+            w_domain="jtag",
+            r_domain="sync")
+
+        m.d.comb += [
+            self.rx_data.eq(rx_fifo.r_data),
+            rx_fifo.r_en.eq(self.rx_stb),
+            self.rx_rdy.eq(rx_fifo.r_rdy),
+        ]
+
+        sel = Signal()
+
+        data_sr = Signal(8)
+
+        with m.If(capture1):
+            m.d.jtag += data_sr.eq(Mux(tx_fifo.r_rdy, tx_fifo.r_data, 0xFF))
+            m.d.jtag += tx_fifo.r_en.eq(1)
+            m.d.jtag += sel.eq(0)
+        with m.Else():
+            m.d.jtag += tx_fifo.r_en.eq(0)
+        with m.If(capture2):
+            m.d.jtag += sel.eq(1)
+
+        m.d.comb += tdo.eq(data_sr)
+        with m.If(shift | update):
+            m.d.jtag += data_sr.eq(Cat(data_sr[1:], tdi))
+
+        update_d = Signal();
+        m.d.jtag += update_d.eq(update)
+        update_negedge = update_d & ~update
+        do_write = Signal()
+
+        with m.If(update_negedge & sel):
+            m.d.jtag += rx_fifo.w_data.eq(data_sr)
+            m.d.jtag += rx_fifo.w_en.eq(1)
+        with m.Else():
+            m.d.jtag += rx_fifo.w_en.eq(0)
+
+        return m
+
+
+class JTAGDebugProbe(wiring.Component):
+    wb_bus: Out(wishbone.Signature(addr_width=24, data_width=8))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.jtagg = jtagg = JTAGG()
+
+        with m.If(jtagg.rx_rdy & ~jtagg.tx_stb):
+            m.d.sync += jtagg.tx_data.eq(jtagg.rx_data)
+            m.d.sync += jtagg.rx_stb.eq(1)
+            m.d.sync += jtagg.tx_stb.eq(1)
+        with m.Else():
+            m.d.sync += jtagg.rx_stb.eq(0)
+            m.d.sync += jtagg.tx_stb.eq(0)
+
+        # TODO: Actual read/write commands over JTAG
+
+        return m
