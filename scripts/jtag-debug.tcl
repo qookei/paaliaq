ba0186f2dc5cbb49f4bd082c857657c61ebd675c
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-05-24 04:26:27 +0200

    scripts: Add tcl procedures for CPU tracing

--- scripts/jtag-debug.tcl
+++ scripts/jtag-debug.tcl
@@ -27,3 +27,48 @@ proc peek {addr} {
     tx 0
     return [rx]
 }
+
+
+proc trace_en {} {
+    poke 0x10048 0x04
+}
+
+proc trace_dis {} {
+    poke 0x10048 0x00
+}
+
+proc trace_do_step {} {
+    poke 0x10048 0x0c
+}
+
+proc trace_step {} {
+    # Grab the address and bus state, then tick forward, then grab r/w data
+    set va [peek 0x1004c]
+    set va [expr {$va | ([peek 0x1004d] << 8)}]
+    set va [expr {$va | ([peek 0x1004e] << 16)}]
+
+    set pa [peek 0x10054]
+    set pa [expr {$pa | ([peek 0x10055] << 8)}]
+    set pa [expr {$pa | ([peek 0x10056] << 16)}]
+
+    set bus [peek 0x10049]
+
+    set vpa [expr {$bus & (1 << 0)}]
+    set vda [expr {$bus & (1 << 1)}]
+    set vpb [expr {$bus & (1 << 2)}]
+    set rwb [expr {$bus & (1 << 3)}]
+
+    trace_do_step
+
+    set rdata [peek 0x10051]
+    set wdata [peek 0x10050]
+
+    format "%06x %s %s%s%s%s %s" \
+	$va \
+	[expr {($vda | $vpa) ? [format "%06x" $pa] : "??????"}] \
+	[expr {$vpa ? "P" : "-"}] \
+	[expr {$vda ? "D" : "-"}] \
+	[expr {$vpb ? "-" : "V"}] \
+	[expr {($vda | $vpa) ? ($rwb ? "R" : "W") : "-"}] \
+	[expr {($vda | $vpa) ? [format "%02x" [expr {$rwb ? $rdata : $wdata}]] : "??"}]
+}
