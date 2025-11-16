2f4d1956e9c196b915e1a5f776d92ae3d533d947
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-11-16 03:14:20 +0100

    rtl: Modify to support 16-bit data w/ DQM
    Needed for the iCESugar-pro SoM, which has an SDRAM chip with a 16-bit
    data bus, and properly wired up DQM (as opposed to hard-wired to 0 on
    the Colorlight boards).

--- rtl/sdram.py
+++ rtl/sdram.py
@@ -11,9 +11,10 @@ class SDRAMSignature(wiring.Signature):
     def __init__(self):
         super().__init__({
             'ba':   Out(2),
-            'a':    Out(11),
-            'dq_i': In(32),
-            'dq_o': Out(32),
+            'a':    Out(13),
+            'dq_i': In(16),
+            'dq_o': Out(16),
+            'dqm':  Out(2),
             'we':   Out(1),
             'ras':  Out(1),
             'cas':  Out(1),
@@ -35,19 +36,23 @@ class SDRAMConnector(wiring.Component):
         sdram = platform.request("sdram", dir="-")
 
         m.submodules.clk = clk = io.Buffer("o", sdram.clk)
+        m.submodules.cke = cke = io.Buffer("o", sdram.clk_en)
         m.submodules.ba = ba = io.Buffer("o", sdram.ba)
         m.submodules.a = a = io.Buffer("o", sdram.a)
         m.submodules.dq = dq = io.Buffer("io", sdram.dq)
+        m.submodules.dqm = dqm = io.Buffer("o", sdram.dqm)
         m.submodules.we = we = io.Buffer("o", sdram.we)
         m.submodules.ras = ras = io.Buffer("o", sdram.ras)
         m.submodules.cas = cas = io.Buffer("o", sdram.cas)
 
         m.d.comb += [
             clk.o.eq(ClockSignal(self._sdram_domain)),
+            cke.o.eq(1),
             ba.o.eq(self.sdram.ba),
             a.o.eq(self.sdram.a),
             dq.oe.eq(self.sdram.we),
             dq.o.eq(self.sdram.dq_o),
+            dqm.o.eq(self.sdram.dqm),
             self.sdram.dq_i.eq(dq.i),
             we.o.eq(self.sdram.we),
             ras.o.eq(self.sdram.ras),
@@ -123,13 +128,14 @@ class SDRAMController(wiring.Component):
                 self.sdram.a.eq(column),
             ]
 
-        def write(bank, column, data):
+        def write(bank, column, data, sel):
             return [
                 self.sdram.ras.eq(0),
                 self.sdram.cas.eq(1),
                 self.sdram.we.eq(1),
                 self.sdram.ba.eq(bank),
                 self.sdram.dq_o.eq(data),
+                self.sdram.dqm.eq(~sel),
                 self.sdram.a.eq(column),
             ]
 
@@ -148,41 +154,31 @@ class SDRAMController(wiring.Component):
         init_clks = ns_to_clks(200000)
         init_ctr = Signal(range(init_clks + 1))
 
-        tRP = 18
-        precharge_clks = ns_to_clks(tRP) + 1
+        precharge_clks = 3 # tRP
         precharge_ctr = Signal(range(precharge_clks + 1))
 
-        tRFC = 60
-        refresh_clks = ns_to_clks(tRFC)
+        refresh_clks = 10 # tRFC
         refresh_ctr = Signal(range(refresh_clks + 1))
         init_refreshes = Signal(1)
 
-        tRCD = 18
-        activate_clks = ns_to_clks(tRCD) + 1
+        activate_clks = 3 # tRCD
         activate_ctr = Signal(range(activate_clks + 1))
 
-        write_clks = 2 # tRDL
+        write_clks = 2 # tDPL
         write_ctr = Signal(range(write_clks + 1))
 
         cas_clks = 2
         read_ctr = Signal(range(cas_clks + 1))
 
         tRES = 64000000
-        refreshes_per_tRES = 4096
+        refreshes_per_tRES = 8192
         time_between_refreshes = tRES // refreshes_per_tRES
         refresh_stb_clks = ns_to_clks(time_between_refreshes)
         refresh_stb_ctr = Signal(range(refresh_stb_clks + 1))
         # Currently we can slip by 1 refresh if the time comes during
         # a read/write.  If that happens, we will do one immediately
-        # after the operation completes (AIUI the refreshes don't need
-        # to happen at exactly when the counter indicates, as long as
-        # we manage to do 4096 of them within 64ms).
+        # after the operation completes.
 
-        # TODO(qookie): Verify that we indeed make it in time?
-        # I doubt we miss anything, and even if we slip a bit, the
-        # chip in the board I have seems to have some sort of
-        # superpower, and retains data for a lot longer than the 64ms
-        # refresh cycle time.
         pending_refresh = Signal()
 
         with m.If(refresh_stb_ctr == refresh_stb_clks):
@@ -201,10 +197,10 @@ class SDRAMController(wiring.Component):
         trans_ack = Signal()
         m.d.comb += self.wb_bus.ack.eq(trans_ack & self.wb_bus.stb)
 
-        byte = self.wb_bus.adr.bit_select(0, 2)
-        column = self.wb_bus.adr.bit_select(2, 8)
-        row = self.wb_bus.adr.bit_select(10, 11)
-        bank = self.wb_bus.adr.bit_select(21, 2)
+        byte = self.wb_bus.adr.bit_select(0, 1)
+        column = self.wb_bus.adr.bit_select(1, 9)
+        row = self.wb_bus.adr.bit_select(10, 12)
+        bank = self.wb_bus.adr.bit_select(22, 2)
 
         banks_active = Signal(4)
         current_rows = Array([Signal(8) for _ in range(4)])
@@ -244,6 +240,7 @@ class SDRAMController(wiring.Component):
                 m.next = 'idle'
             # Main state machine
             with m.State('idle'):
+                m.d.sync += self.sdram.dqm.eq(0)
                 m.d.sync += trans_ack.eq(0)
                 m.d.sync += noop()
                 with m.If(pending_refresh):
@@ -261,11 +258,14 @@ class SDRAMController(wiring.Component):
                     # The target row is already active in the target bank.
                     with m.If(banks_active.bit_select(bank, 1) & (row == current_rows[bank])):
                         # Go do the read/write immediately.
-                        m.d.sync += read_ctr.eq(0)
-                        m.d.sync += read(bank, column)
                         with m.If(self.wb_bus.we):
-                            m.next = 'read-before-write-data'
+                            m.d.sync += write_ctr.eq(0)
+                            byte_val  = self.wb_bus.dat_w << (byte * 8)
+                            m.d.sync += write(bank, column, byte_val, 1 << byte)
+                            m.next = 'write-data'
                         with m.Else():
+                            m.d.sync += read_ctr.eq(0)
+                            m.d.sync += read(bank, column)
                             m.next = 'read-data'
                     # The target bank is idle.
                     with m.Elif(~banks_active.bit_select(bank, 1)):
@@ -293,11 +293,14 @@ class SDRAMController(wiring.Component):
                 with m.If(activate_ctr == activate_clks):
                     m.d.sync += banks_active.bit_select(bank, 1).eq(1)
                     m.d.sync += current_rows[bank].eq(row)
-                    m.d.sync += read_ctr.eq(0)
-                    m.d.sync += read(bank, column)
                     with m.If(self.wb_bus.we):
-                        m.next = 'read-before-write-data'
+                        m.d.sync += write_ctr.eq(0)
+                        byte_val  = self.wb_bus.dat_w << (byte * 8)
+                        m.d.sync += write(bank, column, byte_val, 1 << byte)
+                        m.next = 'write-data'
                     with m.Else():
+                        m.d.sync += read_ctr.eq(0)
+                        m.d.sync += read(bank, column)
                         m.next = 'read-data'
             with m.State('read-data'):
                 m.d.sync += noop()
@@ -307,15 +310,6 @@ class SDRAMController(wiring.Component):
                     m.d.sync += trans_ack.eq(1)
                     m.d.sync += trans_pending.eq(0)
                     m.next = 'idle'
-            with m.State('read-before-write-data'):
-                m.d.sync += noop()
-                m.d.sync += read_ctr.eq(read_ctr + 1)
-                with m.If(read_ctr == cas_clks):
-                    m.d.sync += write_ctr.eq(0)
-                    byte_mask = ~(C(0xFF, 32) << (byte * 8))
-                    byte_val  = self.wb_bus.dat_w << (byte * 8)
-                    m.d.sync += write(bank, column, (self.sdram.dq_i & byte_mask) | byte_val)
-                    m.next = 'write-data'
             with m.State('write-data'):
                 m.d.sync += noop()
                 m.d.sync += write_ctr.eq(write_ctr + 1)
