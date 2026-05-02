a25aacd6ab15d577c2f514068051d62a4322e557
Kacper Słomiński <kacper.slominski72@gmail.com>
2026-05-02 19:58:43 +0200

    rtl: Move clock generation into separate module

--- rtl/paaliaq/artix7/top.py
+++ rtl/paaliaq/artix7/top.py
@@ -1,15 +1,9 @@
 from amaranth import *
-from amaranth.lib import wiring, io
-from amaranth.build import *
-from amaranth.vendor import XilinxPlatform
-from amaranth_boards.resources import *
+from amaranth.lib import io
 
 from paaliaq.soc import SoC
 
-from paaliaq.hdmi import DMT_MODE_1024x768_60Hz
-
-from paaliaq.artix7.pll import S7MMCM
-from paaliaq.artix7.platform import PaaliaqPlatform
+from paaliaq.artix7.crg import CRG
 
 from paaliaq.cpu import P65C816SoftCore
 from paaliaq.sdram import SDRAMConnector
@@ -24,28 +18,12 @@ class PaaliaqTop(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        m.domains.clk50 = cd_clk50 = ClockDomain(platform.default_clk)
-        m.submodules.clk = clk = io.Buffer("i", platform.request(platform.default_clk, dir="-"))
-        m.d.comb += ClockSignal(platform.default_clk).eq(clk.i)
-
-        clk_freq = platform.default_clk_frequency
-
         m.domains.sync = cd_sync = ClockDomain("sync")
         m.domains.sdram = cd_sdram = ClockDomain("sdram")
         m.domains.tmds = cd_tmds = ClockDomain("tmds")
         m.domains.pixel = cd_pixel = ClockDomain("pixel")
 
-        m.submodules.soc_pll = soc_pll = S7MMCM()
-        soc_pll.add_input(clk=clk.i, freq=clk_freq)
-        soc_pll.add_primary_output(freq=self._target_clk)
-        soc_pll.add_secondary_output(freq=self._target_clk, phase=180, domain="sdram")
-
-        mode = DMT_MODE_1024x768_60Hz
-
-        m.submodules.video_pll = video_pll = S7MMCM()
-        video_pll.add_input(clk=clk.i, freq=clk_freq)
-        video_pll.add_primary_output(freq=mode.pixel_clock * 5, domain="tmds")
-        video_pll.add_secondary_output(freq=mode.pixel_clock, domain="pixel")
+        m.submodules.crg = CRG()
 
         m.submodules.sdram_conn = sdram_conn = SDRAMConnector()
         m.submodules.w65c816_conn = w65c816_conn = P65C816SoftCore()
