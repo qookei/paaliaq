c760f5bc01704497eb2bdb7665358ac69d9c60ef
Kacper Słomiński <kacper.slominski72@gmail.com>
2026-05-02 19:36:39 +0200

    rtl: Split build code from top-level module

--- rtl/paaliaq/artix7/top.py
+++ rtl/paaliaq/artix7/top.py
@@ -1,5 +1,3 @@
-import argparse, math
-
 from amaranth import *
 from amaranth.lib import wiring, io
 from amaranth.build import *
@@ -16,7 +14,8 @@ from paaliaq.artix7.platform import PaaliaqPlatform
 from paaliaq.cpu import P65C816SoftCore
 from paaliaq.sdram import SDRAMConnector
 
-class TopLevel(Elaboratable):
+
+class PaaliaqTop(Elaboratable):
     def __init__(self, *, target_clk=75e6, external_cpu=False, boot_rom_path):
         super().__init__()
         self._target_clk = target_clk
@@ -67,25 +66,3 @@ class TopLevel(Elaboratable):
         ]
 
         return m
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--external-cpu', action='store_true')
-    parser.add_argument('--allow-timing-fail', action='store_true')
-    parser.add_argument('--target-clk', type=int, default=75)
-    parser.add_argument("--boot-rom", type=str, default="../build/boot0.bin")
-    parser.add_argument("--build-dir", type=str, default="build")
-
-    args = parser.parse_args()
-    platform = PaaliaqPlatform()
-    with open("external/P65C816.v", "r") as f:
-        platform.add_file("P65C816.v", f)
-    platform.build(
-        TopLevel(
-            external_cpu=args.external_cpu,
-            target_clk=args.target_clk * 1e6,
-            boot_rom_path=args.boot_rom,
-        ),
-        build_dir=args.build_dir,
-    )
