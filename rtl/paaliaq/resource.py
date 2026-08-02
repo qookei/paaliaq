333ec4e35974e80f1c577fff2d87c64c8917bc8b
Kacper Słomiński <kacper.slominski72@gmail.com>
2026-08-02 13:02:02 +0200

    rtl: Add GMII and MDIO resources

--- rtl/paaliaq/resource.py
+++ rtl/paaliaq/resource.py
@@ -51,3 +51,56 @@ def SPIResource(*args, cs_n, clk, dq0, dq1, dq2, dq3, conn=None, attrs=None):
     if attrs is not None:
         io.append(attrs)
     return Resource.family(*args, default_name="spi", ios=io)
+
+
+def GMIIResource(
+    *args,
+    phyrst,
+    rx_clk,
+    rx_dv,
+    rx_er,
+    rxd,
+    gtx_clk,
+    tx_clk=None,
+    tx_en,
+    tx_er,
+    txd,
+    crs,
+    col,
+    conn=None,
+    attrs=None,
+):
+    io = []
+
+    io.append(Subsignal("phyrst", PinsN(phyrst, dir="o", conn=conn, assert_width=1)))
+
+    io.append(Subsignal("rx_clk", Pins(rx_clk, dir="i", conn=conn, assert_width=1)))
+    io.append(Subsignal("rx_dv",  Pins(rx_dv,  dir="i", conn=conn, assert_width=1)))
+    io.append(Subsignal("rx_er",  Pins(rx_er,  dir="i", conn=conn, assert_width=1)))
+    io.append(Subsignal("rxd",    Pins(rxd,    dir="i", conn=conn, assert_width=8)))
+
+    io.append(Subsignal("gtx_clk", Pins(gtx_clk, dir="o", conn=conn, assert_width=1)))
+    io.append(Subsignal("tx_en",   Pins(tx_en,   dir="o", conn=conn, assert_width=1)))
+    io.append(Subsignal("tx_er",   Pins(tx_er,   dir="o", conn=conn, assert_width=1)))
+    io.append(Subsignal("txd",     Pins(txd,     dir="o", conn=conn, assert_width=8)))
+
+    if tx_clk is not None:
+        io.append(Subsignal("tx_clk", Pins(tx_clk, dir="o", conn=conn, assert_width=1)))
+
+    io.append(Subsignal("crs", PinsN(crs, dir="i", conn=conn, assert_width=1)))
+    io.append(Subsignal("col", PinsN(col, dir="i", conn=conn, assert_width=1)))
+
+    if attrs is not None:
+        io.append(attrs)
+    return Resource.family(*args, default_name="gmii", ios=io)
+
+
+def MDIOResource(*args, mdc, mdio, conn=None, attrs=None):
+    io = []
+
+    io.append(Subsignal("mdc",  Pins(mdc,  dir="o",  conn=conn, assert_width=1)))
+    io.append(Subsignal("mdio", Pins(mdio, dir="io", conn=conn, assert_width=1)))
+
+    if attrs is not None:
+        io.append(attrs)
+    return Resource.family(*args, default_name="mdio", ios=io)
